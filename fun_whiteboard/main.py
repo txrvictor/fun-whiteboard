e34a475dab0645588dfebe8553efa6eee90bb8e9
Victor Teixeira <teixeira.victorv@gmail.com>
2024-01-12 20:55:50 +0900

    Refactored global variables and created a whiteboard class

--- fun_whiteboard/main.py
+++ fun_whiteboard/main.py
@@ -9,12 +9,14 @@ def main():
     canvas = tk.Canvas(root, bg = "white")
     canvas.pack(fill = tk.BOTH, expand = True)
 
+    whiteboard = wb.Whiteboard(canvas)
+
     controls_frame = tk.Frame(root)
     controls_frame.pack(side = tk.TOP, fill = tk.X)
 
     color_button = tk.Button(
             controls_frame, text = "Change Color", 
-            command = wb.change_drawing_color)
+            command = whiteboard.change_drawing_color)
     color_button.pack(side = tk.LEFT, padx = 5, pady = 5)
 
     clear_button = tk.Button(
@@ -27,16 +29,13 @@ def main():
 
     line_width_slider = tk.Scale(
             controls_frame, from_ = 1, to = 10,
-            orient = tk.HORIZONTAL, command = lambda val: wb.change_line_width(val))
-    line_width_slider.set(wb.line_width)
+            orient = tk.HORIZONTAL, command = lambda val: whiteboard.change_line_width(val))
+    line_width_slider.set(whiteboard.line_width)
     line_width_slider.pack(side = tk.LEFT, padx = 5, pady = 5)
 
-    def draw_on_canvas(event):
-        wb.draw(event, canvas)
-
-    canvas.bind("<Button-1>", wb.start_drawing)
-    canvas.bind("<B1-Motion>", draw_on_canvas)
-    canvas.bind("<ButtonRelease-1>", wb.stop_drawing)
+    canvas.bind("<Button-1>", whiteboard.start_drawing)
+    canvas.bind("<B1-Motion>", whiteboard.draw)
+    canvas.bind("<ButtonRelease-1>", whiteboard.stop_drawing)
 
     root.mainloop()
 
--- fun_whiteboard/whiteboard.py
+++ fun_whiteboard/whiteboard.py
@@ -1,36 +1,35 @@
 from tkinter import ROUND
 from tkinter.colorchooser import askcolor
 
-is_drawing = False
-drawing_color = "black"
-line_width = 2
+class Whiteboard:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.is_drawing = False
+        self.drawing_color = "black"
+        self.line_width = 2
 
-def start_drawing(event):
-    global is_drawing, prev_x, prev_y
-    is_drawing = True
-    prev_x, prev_y = event.x, event.y
+    def start_drawing(self, event):
+        self.is_drawing = True
+        self.prev_x, self.prev_y = event.x, event.y
 
-def draw(event, canvas):
-    global is_drawing, prev_x, prev_y
-    if is_drawing:
-        current_x, current_y = event.x, event.y
-        canvas.create_line(
-            prev_x, prev_y, current_x, current_y, 
-            fill = drawing_color, width = line_width,
-            capstyle = ROUND, joinstyle = ROUND,
-            smooth = True)
-        prev_x, prev_y = current_x, current_y
-        
-def stop_drawing(_):
-    global is_drawing
-    is_drawing = False
+    def draw(self, event):
+        if self.is_drawing:
+            current_x, current_y = event.x, event.y
+            self.canvas.create_line(
+                self.prev_x, self.prev_y,
+                current_x, current_y, 
+                fill = self.drawing_color, width = self.line_width,
+                capstyle = ROUND, joinstyle = ROUND,
+                smooth = True)
+            self.prev_x, self.prev_y = current_x, current_y
+            
+    def stop_drawing(self, _):
+        self.is_drawing = False
 
-def change_drawing_color():
-    global drawing_color
-    color = askcolor(initialcolor = drawing_color)[1]
-    if color:
-        drawing_color = color
+    def change_drawing_color(self):
+        color = askcolor(initialcolor = self.drawing_color)[1]
+        if color:
+            self.drawing_color = color
 
-def change_line_width(value):
-    global line_width
-    line_width = int(value)
+    def change_line_width(self, value):
+        self.line_width = int(value)
